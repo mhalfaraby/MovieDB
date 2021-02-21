42672c6421130a957682f3ca669bf373dbc7e27b
Muhammad Alfaraby <mhalfaraby@gmail.com>
2021-02-21 14:29:52 +0700

    add runtime

--- MovieDB/Controller/DetailViewController.swift
+++ MovieDB/Controller/DetailViewController.swift
@@ -15,6 +15,7 @@ class DetailViewController: UIViewController {
   @IBOutlet weak var genre2: UILabel!
   @IBOutlet weak var genre3: UILabel!
   @IBOutlet weak var detailTitle: UILabel!
+  @IBOutlet weak var runtime: UILabel!
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var imagePoster: UIImageView!
   @IBOutlet weak var detailOverview: UITextView!
@@ -29,7 +30,7 @@ class DetailViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-  
+    
     fetchDetail(id: detailId!)
     fetchCast(id: detailId!)
     
@@ -37,8 +38,8 @@ class DetailViewController: UIViewController {
     
     
     collectionView.reloadData()
-
-   
+    
+    
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -58,7 +59,7 @@ class DetailViewController: UIViewController {
     request.responseDecodable(of: Detail.self) { (response) in
       guard let result = response.value else { return }
       let name = result.poster_path
-       let url = "https://image.tmdb.org/t/p/w500//\(String(describing: name))"
+      let url = "https://image.tmdb.org/t/p/w500//\(String(describing: name))"
       let urlToImage = NSURL.init(string: url)
       self.imagePoster.af.setImage(withURL: urlToImage! as URL)
       
@@ -66,14 +67,18 @@ class DetailViewController: UIViewController {
       self.detailOverview.text = result.overview
       
       
+      
+      let hours = result.runtime / 60
+      let minutes = result.runtime % 60
+      self.runtime.text = ("\(hours)h \(minutes)m")
+      
       self.genre.append(contentsOf: result.genres.map{$0.name})
       
-        self.genre1.text = self.genre[0]
+      self.genre1.text = self.genre[0]
       
       let intendedIndex: Int = 1
-
+      
       if (intendedIndex >= 0 && self.genre.count > intendedIndex) {
-          // This line will not throw index out of range:
         let genre2 = self.genre[intendedIndex]
         
         self.genre2.text = genre2
@@ -81,39 +86,24 @@ class DetailViewController: UIViewController {
         self.genre3.text = ""
       }
       
-      
-      
-      
       let intendedIndex2: Int = 2
-
+      
       if (intendedIndex >= 0 && self.genre.count > intendedIndex2) {
-          // This line will not throw index out of range:
         let genre3 = self.genre[intendedIndex2]
         
         self.genre3.text = genre3
       } else {
         self.genre3.text = ""
       }
-      
-      
-    
 
-//
-//          self.genre2.text = self.genre[i+1]
-//          self.genre3.text = self.genre[i+2]
-      
-      
-     
-
-        
-      }
-      
-
-
-      
-      
     }
-
+    
+    
+    
+    
+    
+  }
+  
   
   
   func fetchCast(id: Int) {
@@ -122,33 +112,33 @@ class DetailViewController: UIViewController {
     request.responseDecodable(of: Cast.self) { (response) in
       guard let result = response.value else { return }
       
- self.detailCast = [result]
-result.cast.forEach { (Casts) in
- if let cast = Casts.profile_path {
+      self.detailCast = [result]
+      result.cast.forEach { (Casts) in
+        if let cast = Casts.profile_path {
           self.countCast.append(cast)
         }
       }
       DispatchQueue.main.async {
         self.collectionView.reloadData()
       }
+    }
+    
+    
+    
+    
+    
+    
   }
   
-      
-    
-    
-    
-    
-  }
-
 }
 
 
 
 extension DetailViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
+    
     return countCast.count
-
+    
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -161,14 +151,14 @@ extension DetailViewController: UICollectionViewDataSource {
       let url = "https://image.tmdb.org/t/p/w500/\(name)"
       let urlToImage = NSURL.init(string: url)
       cell.castImage.af.setImage(withURL: urlToImage! as URL)
-
+      
     } else {
       cell.castImage.image = UIImage(systemName: "person.circle")
-
+      
     }
     
     cell.castName.text = detailCast[0].cast[indexPath.row].name
-
+    
     
     return cell
   }
